dccbe1838ba97ac12858bef1ca21285ee2f89c0d
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2023-01-04 13:25:10 +1300

    fixed display issue on mobile with cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -39,7 +39,7 @@ const HeaderCartButton = props => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes.btnLabel}>Your Cart</span>
       <span className={classes.badge}>{cartItems}</span>
     </button>
   );
